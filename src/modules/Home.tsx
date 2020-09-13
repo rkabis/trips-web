2f28d49f74f77330a427e38f73bd558e4b80d5ba
Ryan Abis <ryan.abis@gmail.com>
2020-09-13 15:30:34 +0800

    add UI components

--- src/modules/Home.tsx
+++ src/modules/Home.tsx
@@ -2,15 +2,30 @@ import React, { ReactElement } from 'react'
 
 import { makeStyles } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     flexDirection: 'column',
-    backgroundColor: 'black',
     height: '100vh',
     width: '100%',
     alignItems: 'center'
+  },
+  dateField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 200
+  },
+  dateRow: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(2)
+  },
+  button: {
+    marginTop: theme.spacing(1)
   }
 }))
 
@@ -19,7 +34,26 @@ const Home = (): ReactElement => {
 
   return (
     <div className={classes.root}>
-      <Button variant='contained'>
+      <div className={classes.dateRow}>
+        <TextField
+          label={'FROM'}
+          type="datetime-local"
+          className={classes.dateField}
+          InputLabelProps={{
+            shrink: true
+          }}
+        />
+        <TextField
+          label={'TO'}
+          type="datetime-local"
+          className={classes.dateField}
+          InputLabelProps={{
+            shrink: true
+          }}
+        />
+      </div>
+      <TextField id="standard-basic" label={'Device ID'} />
+      <Button variant='contained' className={classes.button}>
         {'SEARCH LOGS'}
       </Button>
     </div>
